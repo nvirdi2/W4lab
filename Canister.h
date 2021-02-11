3d24e283f8e8e0ccafd4968178232cf276e6b4c3
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-02-11 18:35:41 -0500

    Create Canister.h

---
+++ Canister.h
@@ -0,0 +1,38 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: Feb 11, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef SDDS_CANISTER_H
+#define SDDS_CANISTER_H
+#include <iostream>
+namespace sdds {
+   class Canister {
+      char* m_contentName;
+      double m_diameter; // in centimeters 
+      double m_hieght;   // in centimeters
+      double m_contentVolume;  // in CCs
+      bool m_usable;
+      void setToDefault();
+      void setName(const char* Cstr);
+      bool isEmpty()const;
+      bool hasSameContent(const Canister& C)const;
+   public:
+      Canister();
+      Canister(const char* contentName);
+      Canister(double hieght, double diameter,
+         const char* contentName = nullptr);
+      ~Canister();
+      Canister& setContent(const char* contentName);
+      Canister& pour(double quantity);
+      Canister& pour(Canister&);
+      double volume()const;
+      std::ostream& display()const;  //!
+      double capacity()const;
+      void clear();
+   };
+}
+
+#endif // !SDDS_BOX_H
